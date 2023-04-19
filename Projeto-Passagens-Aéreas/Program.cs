3644bb920d677620e0f34261b11b13eab86c0292
Mateus Moura Ferreira <48527217856@info.local>
2023-04-19 09:04:43 -0300

    alteracoes do dia 19-04

---
+++ Projeto-Passagens-Aéreas/Program.cs
@@ -0,0 +1,164 @@
+﻿string[] nome = new string[5];
+string[] origem = new string[5];
+string[] Destino = new string[5];
+string[] data = new string[5];
+
+int senha = 123456;
+
+// return true ; 
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Bem vindo a companhia aerea de maua!");
+
+
+    Console.WriteLine(@$"
+    1-Para cadastrar
+    0-Para sair.");
+    char cadastrar = char.Parse(Console.ReadLine());
+
+    while (cadastrar != '0')
+    {
+
+
+        if (cadastrar == '1')
+        {
+            Console.WriteLine($"Insira sua senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            if (senha != 123456)
+            {
+                Console.WriteLine($"Senha incorreta!");
+
+                // return false ;
+
+            }
+            else
+            {
+                Console.WriteLine($"Senha correta!");
+                Console.WriteLine($"Insira seu nome: ");
+                nome[i] = Console.ReadLine();
+
+                Console.WriteLine($"Origem: ");
+                origem[i] = Console.ReadLine();
+
+                Console.WriteLine($"Destino: ");
+                Destino[i] = Console.ReadLine();
+
+                Console.WriteLine($"Data do voo: ");
+                data[i] = Console.ReadLine();
+
+                Console.WriteLine($"Sua passagem foi cadastrada, se desejar listar ela, digite '2', senao digite '0' para sair");
+                cadastrar = char.Parse(Console.ReadLine());
+                if (cadastrar == '2')
+                {
+                    Console.WriteLine(@$"
+    Passagem #{i}
+    nome: {nome[i]} 
+    origem: {origem[i]} 
+    Destino: {Destino[i]} 
+    Data do voo: {data[i]}.
+    Digite 0 para cadastrar outro passageiro ");
+                    cadastrar = char.Parse(Console.ReadLine());
+                }
+                else if (cadastrar == '0')
+                {
+                    Console.WriteLine($"Voce saiu com sucesso!");
+
+                }
+                // char cadastrar = char.Parse(Console.ReadLine());
+
+                else
+                {
+                    Console.WriteLine($"Codigos digitados errados.");
+                }
+            }
+        }
+    }
+}
+
+
+// }
+// else
+// {
+
+
+// }
+
+// }
+
+// Console.WriteLine($"Insira sua senha: ");
+// senha = int.Parse(Console.ReadLine());
+
+
+// static bool senhaVerdadeira(int senha)
+// {
+
+// }
+
+
+// if (senha != 123456)
+// {
+//     Console.WriteLine($"Senha incorreta!");
+
+//     // return false ;
+
+// }
+// else
+// {
+//     Console.WriteLine($"Senha correta!");
+//     // return true ; 
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         Console.WriteLine($"Insira seu nome: ");
+//         nome[i] = Console.ReadLine();
+
+//         Console.WriteLine($"Origem: ");
+//         origem[i] = Console.ReadLine();
+
+//         Console.WriteLine($"Destino: ");
+//         Destino[i] = Console.ReadLine();
+
+//         Console.WriteLine($"Data do voo: ");
+//         data[i] = Console.ReadLine();
+
+//         Console.WriteLine(@$"1-Para cadastrar.
+// 2- Para listar passagem.
+// 0-Para sair.");
+//         char cadastrar = char.Parse(Console.ReadLine());
+//         }
+
+
+// }
+// while (cadastrar !=0)
+// {
+
+
+//         switch (cadastrar)
+//         {
+//             case '1':
+//                 Console.WriteLine($"Sua passagem foi cadastrada. Deseja listar a passagem? Digite 2 se sim, ou 0 pra sair!");
+//                 cadastrar = char.Parse(Console.ReadLine());
+//                 break;
+
+//             case '2':
+//                 Console.WriteLine(@$"nome: {nome[i]} 
+//     origem: {origem[i]} 
+//     Destino: {Destino[i]} 
+//     Data do voo: {data[i]}.
+//     Deseja sair? digite 0
+//     ");
+//     cadastrar = char.Parse(Console.ReadLine());
+//     break;
+//             case '0':
+//                 break;
+
+//             default:
+//             Console.WriteLine($"Erro nos codigos digitados.");
+
+//                 break;
+//         }
+// }
+
+
+
+
---
+++ Projeto-promocao/Program.cs
@@ -0,0 +1,142 @@
+﻿string[] nome = new string[10];
+float[] preco = new float[10];
+bool[] promocao = new bool[10];
+// promocao = true = Console.WriteLine($"O produto esta em promocao.");
+// promocao = false = Console.WriteLine($"O produto nao esta em promocao.");
+char opcao ='a';
+static char menu()
+{
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.WriteLine(@$"O que deseja fazer:
+    1- Para cadastrar produtos
+    2- Listar produtos (Voce so podera listar, se ja tiver cadastrado algum produto.)
+    0- para sair.
+    4- para Mostrar o Menu novamente
+    ");
+    Console.ResetColor();
+    return char.Parse(Console.ReadLine());
+
+}
+do
+{
+
+ opcao=menu();
+    
+
+    switch (opcao)
+    {
+        case '1':
+            for (int i = 0; i < 10; i++)
+            {
+                //     Console.WriteLine(@$"Deseja cadastrar {i+1}ª Produto
+                //     Se sim, Digite 'S'
+                //     Senao, Digite  'N' ?");
+                //     char cadastrar = char.Parse(Console.ReadLine());
+
+                // if (cadastrar == 'S')
+                // {
+
+
+                Console.WriteLine($"Digite o nome do {i + 1}ª produto: ");
+                nome[i] = Console.ReadLine();
+                Console.WriteLine($"Qual o preco do {i + 1}ª produto: ");
+                preco[i] = float.Parse(Console.ReadLine());
+
+                Console.WriteLine(@$"
+O {i + 1}ª produto esta em promocao?
+Digite 'S' para Sim
+Digite 'N' para Nao");
+                char respostaPromocao = char.Parse(Console.ReadLine());
+                if (respostaPromocao == 'S')
+                {
+                    promocao[i] = true;
+
+                }
+                else if (respostaPromocao == 'N')
+                {
+                    promocao[i] = false;
+                    // if(promocao[i]==true)
+                    // {
+                    //     Console.WriteLine($"O produto esta em promocao.");
+
+                    // }
+                    // else
+                    // {
+                    //     Console.WriteLine($"O produto nao esta em promocao.");
+
+                    // }
+                }
+
+                else
+                {
+
+                    Console.WriteLine($"Voce digitou a letra errada!");
+
+                }
+            }
+            break;
+        case '2':
+            for (int i = 0; i < 10; i++)
+            {
+                // Console.WriteLine($"Voce cadastrou {i+1} produtos!");
+
+                if (promocao[i] == true)
+                {
+                    Console.WriteLine($" {i + 1}ª Produto: {nome[i]} custa R${preco[i]}, e Esta em promocao!");
+                }
+                else if (promocao[i] == false)
+                {
+                    Console.WriteLine($" {i + 1}ª Produto: {nome[i]} custa R${preco[i]}, e Nao esta em promocao!");
+                }
+
+                // Console.WriteLine($" {i+1}ª Produto: {nome[i]} custa {preco[i]}, e {promocao[i]}");
+                // Console.WriteLine($" {i}ª Produto: {nome[i]} custa {preco[i]}, e {promocao[i]}");
+            }
+            break;
+
+    }
+
+} while (opcao != '0');
+
+//  else if(cadastrar == 'N')
+//  {
+//     Console.WriteLine($"Ok! ");
+//     break;
+//     }
+// for (int i = 1; i <= 10; i++)
+// {
+
+// }
+//  }
+
+// Console.WriteLine(@$"Deseja listar os produtos
+//  Digite 'S' para Sim
+//  Digite 'N' para Nao ?");
+// char listar = char.Parse(Console.ReadLine());
+// if (listar == 'S')
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         Console.WriteLine($"Voce cadastrou {i + 1} produtos!");
+
+//         if (promocao[i] == true)
+//         {
+//             Console.WriteLine($" {i + 1}ª Produto: {nome[i]} custa R${preco[i]}, e Esta em promocao!");
+//         }
+//         else if (promocao[i] == false)
+//         {
+//             Console.WriteLine($" {i + 1}ª Produto: {nome[i]} custa R${preco[i]}, e Nao esta em promocao!");
+//         }
+
+//         // Console.WriteLine($" {i+1}ª Produto: {nome[i]} custa {preco[i]}, e {promocao[i]}");
+//         // Console.WriteLine($" {i}ª Produto: {nome[i]} custa {preco[i]}, e {promocao[i]}");
+//     }
+// }
+// else if (listar == 'N')
+// {
+//     Console.WriteLine($"Ok! o programa sera finalizado.");
+
+// }
+
+
+
